9e11301b52c348e03677618593bbf10a31ed31da
Sem-Hacker <DELL@ELIPHAZ-PC.com>
2024-07-31 00:47:22 +0000

    A

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-<img alt="Vue logo" src="./assets/logo.png">
-<HelloWorld msg="  hello word  "/>
+<HelloWorld msg="   hello everyone my name is sem "/>
 </template>
 
 <script>
@@ -19,7 +18,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+<h1>hello word </h1>
 </template>
 
 <script>
